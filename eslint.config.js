d4067f8dce6ed0ce875478a6667aa48830577c94
wtfgn <exal59@yahoo.com>
2024-01-19 12:26:52 +0800

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+// require('@rushstack/eslint-patch/modern-module-resolution')
+import antfu from '@antfu/eslint-config'
+
+// module.exports = {
+//   root: true,
+//   'extends': [
+//     'plugin:vue/vue3-essential',
+//     'eslint:recommended',
+//     '@vue/eslint-config-typescript'
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 'latest'
+//   }
+// }
+
+export default antfu()
