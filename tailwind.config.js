06bc19f712db54d413ce9e0506ef200ecd7f16d4
wtfgn <exal59@yahoo.com>
2024-01-23 15:00:15 +0800

    Some styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ export default {
         '9xl': '96rem',
         '10xl': '104rem',
       },
+      animation: {
+        bounce200: 'bounce 1s infinite 200ms',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': { transform: 'translateY(0)', animationTimingFunction: 'cubic-bezier(0.0,0.0,0.2,1)' },
+          '50%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0.4,0,1,1)' },
+        },
+      },
     },
   },
   plugins: [],
