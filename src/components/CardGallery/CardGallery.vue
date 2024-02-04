d57ddc33b625b1c41f7c33e5329bd24781d1b9a8
wtfgn <exal59@yahoo.com>
2024-02-04 23:39:28 +0800

    Allow custom infiniteScroll element

--- src/components/CardGallery/CardGallery.vue
+++ src/components/CardGallery/CardGallery.vue
@@ -1,8 +1,6 @@
 <template>
   <ContainerTemplate>
     <template #heading>
-      {{ props.filteredCards }}
-      {{ props.fetchError ?? 'NULL' }}
       <slot name="heading">
         <h2 class="text-2xl mb-4 dark:text-white font-semibold">
           Default Heading
@@ -12,34 +10,41 @@
 
     <!-- Main content -->
     <template #default>
-      <slot name="default" :cards-to-display="cardsToDisplay">
+      <slot
+        v-if="!props.fetchError && props.filteredCards"
+        name="default"
+        :cards-to-display="cardsToDisplay"
+      >
         <!-- Default Content -->
         <div
-          v-if="!props.fetchError && props.filteredCards.length"
-          ref="cardsContainerEle"
+
+          ref="defaultCardsContainerEle"
           class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4
           overflow-y-scroll mx-6 h-screen mb-10
           border-b border-slate-900/10 dark:border-slate-50/[0.06] transition-colors"
         >
-          <CardImage
-            v-for="card in cardsToDisplay" :key="card.card_id" :card="card"
-            class="mx-auto"
-          />
+          <slot v-for="card in cardsToDisplay" :key="card.card_id" name="card" :card="card">
+            <div>
+              <CardImage :card="card" />
+            </div>
+          </slot>
         </div>
+      </slot>
 
-        <slot v-else-if="!props.fetchError" name="loading">
-          <div class="flex justify-center items-center h-96">
-            <Spinner class="h-12 w-12 text-sky-600 dark:text-sky-300" />
-          </div>
-        </slot>
+      <!-- Loading -->
+      <slot v-else-if="!props.fetchError" name="loading">
+        <div class="flex justify-center items-center h-96">
+          <Spinner class="h-12 w-12 text-sky-600 dark:text-sky-300" />
+        </div>
+      </slot>
 
-        <slot v-else name="error">
-          <div class="flex justify-center items-center h-96">
-            <p class="text-xl dark:text-white">
-              {{ props.fetchError.message }}
-            </p>
-          </div>
-        </slot>
+      <!-- Error -->
+      <slot v-else name="error">
+        <div class="flex justify-center items-center h-96">
+          <p class="text-xl dark:text-white">
+            {{ props.fetchError.message }}
+          </p>
+        </div>
       </slot>
     </template>
   </ContainerTemplate>
@@ -57,18 +62,27 @@ import type { Card } from '@/types/card';
 
 const props = defineProps({
   filteredCards: {
-    type: Array as PropType<Card[]>,
+    type: Array as PropType<Card[] | null>,
     required: true,
   },
   fetchError: {
     type: Object as PropType<Error | null>,
     default: undefined,
   },
+  cardsContainerEle: {
+    type: Object as PropType<HTMLElement | null>,
+    default: null,
+  },
+  scrollDiection: {
+    type: String as PropType<'top' | 'bottom' | 'left' | 'right'>,
+    default: 'bottom',
+  },
 });
-
-const cardsContainerEle = ref<HTMLElement | null>(null);
 const numToLoad = ref(20);
 const cardsToDisplay = ref<Card[]>([]);
+
+const defaultCardsContainerEle = ref<HTMLElement | null>(null);
+const cardsContainerEle = computed(() => props.cardsContainerEle ?? defaultCardsContainerEle.value);
 
 useInfiniteScroll(cardsContainerEle, (_state) => {
   if (!props.filteredCards)
@@ -81,7 +95,7 @@ useInfiniteScroll(cardsContainerEle, (_state) => {
       cardsToDisplay.value.length + numToLoad.value,
     ),
   ];
-}, { distance: 10 });
+}, { distance: 10, direction: props.scrollDiection });
 
 watchEffect(() => {
   if (!props.filteredCards)
