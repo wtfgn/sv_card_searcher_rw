2ad321db5df639a168e3c2bbae0c1a030360b19d
wtfgn <exal59@yahoo.com>
2024-02-06 17:17:48 +0800

    Fix

--- src/components/Template/ContainerTemplate.vue
+++ src/components/Template/ContainerTemplate.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white dark:bg-gray-800 rounded border border-gray-200 dark:border-gray-700 shadow-lg">
+  <div class="bg-white dark:bg-gray-800 rounded border border-gray-200 dark:border-gray-700 shadow-lg transition-all">
     <slot name="heading" />
 
     <slot name="default" />
