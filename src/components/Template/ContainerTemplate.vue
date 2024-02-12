e78805884c18069b7e5cda5e47b363feaf6aefb5
wtfgn <exal59@yahoo.com>
2024-02-13 00:28:14 +0800

    Change styling method to dynamic class

--- src/components/Template/ContainerTemplate.vue
+++ src/components/Template/ContainerTemplate.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="bg-white dark:bg-gray-800 rounded border border-gray-200 dark:border-gray-700 shadow-lg transition-all">
+  <div
+    class=" rounded border  shadow-lg transition-all"
+    :class="isDark ? 'dark:bg-gray-800 dark:border-gray-700' : 'bg-white border-gray-200 '"
+  >
     <slot name="heading" />
 
     <slot name="default" />
   </div>
 </template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '@/stores/user';
+
+const userStore = useUserStore();
+const { isDark } = storeToRefs(userStore);
+</script>
