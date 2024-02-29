b14f80a6565572f1a18ce30babd2f38f07784f5f
wtfgn <exal59@yahoo.com>
2024-02-29 23:48:44 +0800

    Add new icons

---
+++ src/components/Icons/ExternalLinkIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path fillRule="evenodd" clipRule="evenodd" d="M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6zm10-1a1 1 0 1 0 0 2h2.586l-4.293 4.293a1 1 0 0 0 1.414 1.414L17 8.414V11a1 1 0 1 0 2 0V6a1 1 0 0 0-1-1h-5z" fill="currentColor" /></g></svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MajesticonsExternalLink',
+};
+</script>
