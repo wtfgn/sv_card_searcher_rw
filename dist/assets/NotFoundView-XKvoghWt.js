1c2bced0da268e883fa906048323cc82872367e4
wtfgn <exal59@yahoo.com>
2024-02-29 13:35:15 +0800

    Update dist

---
+++ dist/assets/NotFoundView-XKvoghWt.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as n,a as e}from"./index-vguJV-fA.js";const c={},s=e("h1",null,"404",-1),_=e("p",null,"Page not found",-1),a=[s,_];function r(d,l){return t(),n("div",null,a)}const u=o(c,[["render",r]]);export{u as default};
