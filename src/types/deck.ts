769db6995eae23ff767ae2a344efdc60262657d5
wtfgn <exal59@yahoo.com>
2024-03-05 20:00:33 +0800

    Update interface

--- src/types/deck.ts
+++ src/types/deck.ts
@@ -3,6 +3,6 @@ import type { Card, CardProperty } from './card';
 export interface DeckInStorage {
   name: string
   deckHash: string
-  format: CardProperty
-  clan: CardProperty
+  format?: CardProperty
+  clan: CardProperty | null
 }
